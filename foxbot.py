f824f7bbba6940acb12142c0a18de80cd1606d6c
Bruno Domingues <bruno_signorelli@outlook.com>
2021-04-27 21:11:48 -0300

    Added functions

--- foxbot.py
+++ foxbot.py
@@ -9,11 +9,76 @@ with open("modelo.bin", "rb") as m:
 with open("vectorizer.bin", "rb") as v:
     vectorizer = load(v)
 
-while(True):
-    text_in = input("Opa. Sou o Foxbot. Qual é o seu desejo?")
-    counts = vectorizer.transform([text_in])
-    resp = model.predict(counts)
-    if (np.amax(model.predict_proba(counts)[0]) < 0.6):
-        print("Desculpe, não entendi.\n")
-    else:
-        print (resp)
+possibilities = [i for i in model.classes_]
+
+
+def greetUser(p=possibilities):
+    print("Opa. Sou o Foxbot. Posso responder perguntas relacionadas à: \n")
+    for i in range(len(p)):
+        print(f"{i+1}) {p[i]}")
+    print("\nPara sair basta digitar 'sair' ou apertar Ctrl+C.\n")
+
+
+def processInput(u_input, m=model, v=vectorizer):
+    new_t = v.transform([u_input])
+    pred = model.predict(new_t)
+    if np.amax(model.predict_proba(new_t)[0]) < 0.6:
+        pred = "Desculpe, nao entendi."
+    return pred, new_t
+
+
+def wrongPred(u_input_vectorized, p=possibilities, m=model):
+    print("Selecione o que voce esperava de acordo com as opcoes abaixo: \n")
+
+    for i in range(len(p)):
+        print(f"{i+1}) {p[i]}")
+    print(" ")
+
+    while True:
+        try:
+            expected = int(input("Opcao escolhida: "))
+
+            if expected not in [1, 2, 3]:
+                print("Opcao invalida, por favor, digite novamente.\n")
+                continue
+
+            else:
+                print(p[expected - 1] + "\n")
+                model.partial_fit(u_input_vectorized, [p[expected - 1]], classes=p)
+                print("Obrigado por ajudar a melhorar o FoxBot.")
+                break
+
+        except ValueError:
+            print("Opcao invalida, por favor, digite novamente.\n")
+            continue
+
+
+greetUser()
+
+while True:
+    try:
+        text_in = clean_text(str(input("O que voce gostaria de perguntar? ")))
+
+        if text_in == "Sair" or text_in == "sair":
+            print("\nAdeus.\n")
+            break
+
+        else:
+            pred, transform = processInput(text_in)
+            if pred != "Desculpe, nao entendi.":
+                print("\nEntendi que deseja a seguinte funcao:")
+                print(pred[0] + "\n")
+
+                correct = str(input("Era isto que voce desejava? (s/n) "))
+
+                if correct == "n":
+                    wrongPred(transform)
+                else:
+                    continue
+
+            else:
+                print(pred + "\n")
+
+    except KeyboardInterrupt:
+        print("\nAdeus.\n")
+        break
